9353cdedb50d886ad0a9f23ee774c6e310eb8864
Eugene Sharygin <eush77@gmail.com>
2016-01-24 23:40:03 +0300

    support keys with identity

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 'use strict';
 
+var Map = require('es6-map');
+
 
 module.exports = function (ast, prop) {
-  var index = Object.create(null);
+  var index = new Map;
 
   (function preorder (node) {
     var key = node[prop];
-    index[key] = index[key] || [];
-    index[key].push(node);
+
+    if (!index.has(key)) {
+      index.set(key, []);
+    }
+    index.get(key).push(node);
 
     node.children && node.children.forEach(preorder);
   }(ast));
 
   return {
     get: function (key) {
-      return index[key] || [];
+      return index.get(key) || [];
     }
   };
 };
--- test/test.js
+++ test/test.js
@@ -61,17 +61,46 @@ test('index.get', function (t) {
 });
 
 
-test('Object.prototype keys', function (t) {
-  var ast = u('node', { word: '__proto__', id: 0 }, [
-    u('node', { word: 'constructor', id: 1 }),
-    u('node', { word: 'toString', id: 2 })
-  ]);
-  var $ = select.one(ast);
+test('degenerate keys', function (t) {
+  t.test('Object.prototype keys', function (t) {
+    var ast = u('node', { word: '__proto__', id: 0 }, [
+      u('node', { word: 'constructor', id: 1 }),
+      u('node', { word: 'toString', id: 2 })
+    ]);
+    var $$ = select(ast);
 
-  var index = Index(ast, 'word');
+    var index = Index(ast, 'word');
 
-  t.deepEqual(index.get('__proto__'), [$('[id=0]')]);
-  t.deepEqual(index.get('constructor'), [$('[id=1]')]);
-  t.deepEqual(index.get('toString'), [$('[id=2]')]);
+    t.deepEqual(index.get('__proto__'), $$('[id=0]'));
+    t.deepEqual(index.get('constructor'), $$('[id=1]'));
+    t.deepEqual(index.get('toString'), $$('[id=2]'));
+    t.end();
+  });
+
+  t.test('identity keys', function (t) {
+    var id1 = { foo: 'bar' };
+    var id2 = { foo: 'bar' };
+    var ast = u('root', [
+      u('node', { word: false, id: 0 }),
+      u('node', { word: 'false', id: 1 }),
+      u('node', { word: 1, id: 2 }),
+      u('node', { word: '1', id: 3 }),
+      u('node', { word: id1, id: 4 }),
+      u('node', { word: id2, id: 5 })
+    ]);
+    var $$ = select(ast);
+
+    var index = Index(ast, 'word');
+
+    t.deepEqual(index.get(false), $$('[id=0]'));
+    t.deepEqual(index.get('false'), $$('[id=1]'));
+    t.deepEqual(index.get(1), $$('[id=2]'));
+    t.deepEqual(index.get('1'), $$('[id=3]'));
+    t.deepEqual(index.get(id1), $$('[id=4]'));
+    t.deepEqual(index.get(id2), $$('[id=5]'));
+    t.deepEqual(index.get({ foo: 'bar' }), []);
+    t.end();
+  });
+
   t.end();
 });
