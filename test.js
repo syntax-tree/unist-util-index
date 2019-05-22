45596bc69d2382ebee7cd9af8c80f1a27256b2c9
Titus Wormer <tituswormer@gmail.com>
2019-05-22 08:11:50 +0200

    Update dev-dependencies

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 
 var test = require('tape')
 var u = require('unist-builder')
-var select = require('unist-util-select')
+var select = require('unist-util-select').select
 var Index = require('.')
 
 var index = Index
@@ -93,21 +93,20 @@ test('Index', function(t) {
 test('index.add', function(t) {
   var ast = u('root', [u('node', {word: 'foo'}), u('node', {word: 'bar'})])
   var extraNode = u('node', {word: 'foo'})
-  var $ = select.one(ast)
 
   var index = new Index(ast, 'word')
-  t.deepEqual(index.get('foo'), [$('[word=foo]')])
+  t.deepEqual(index.get('foo'), [select('[word=foo]', ast)])
 
   var result = index.add(extraNode)
-  t.deepEqual(index.get('foo'), [$('[word=foo]'), extraNode])
+  t.deepEqual(index.get('foo'), [select('[word=foo]', ast), extraNode])
 
   t.equal(result, index, 'returns this')
 
-  index.add($('[word=foo]'))
-  t.deepEqual(index.get('foo'), [$('[word=foo]'), extraNode])
+  index.add(select('[word=foo]', ast))
+  t.deepEqual(index.get('foo'), [select('[word=foo]', ast), extraNode])
 
   index.add(extraNode)
-  t.deepEqual(index.get('foo'), [$('[word=foo]'), extraNode])
+  t.deepEqual(index.get('foo'), [select('[word=foo]', ast), extraNode])
 
   t.end()
 })
@@ -136,26 +135,29 @@ test('index.get', function(t) {
         ])
       ])
     ])
-    var $ = select.one(ast)
 
     var index = new Index(ast, 'color')
 
     st.deepEqual(index.get('black'), [
-      $('[id=0]'),
-      $('[id=1]'),
-      $('[id=3]'),
-      $('[id=4]'),
-      $('[id=5]'),
-      $('[id=7]'),
-      $('[id=8]'),
-      $('[id=9]'),
-      $('[id=10]'),
-      $('[id=12]'),
-      $('[id=13]'),
-      $('[id=14]')
+      select('[id=0]', ast),
+      select('[id=1]', ast),
+      select('[id=3]', ast),
+      select('[id=4]', ast),
+      select('[id=5]', ast),
+      select('[id=7]', ast),
+      select('[id=8]', ast),
+      select('[id=9]', ast),
+      select('[id=10]', ast),
+      select('[id=12]', ast),
+      select('[id=13]', ast),
+      select('[id=14]', ast)
     ])
 
-    st.deepEqual(index.get('red'), [$('[id=2]'), $('[id=6]'), $('[id=11]')])
+    st.deepEqual(index.get('red'), [
+      select('[id=2]', ast),
+      select('[id=6]', ast),
+      select('[id=11]', ast)
+    ])
 
     st.deepEqual(index.get('yellow'), [])
 
@@ -168,12 +170,11 @@ test('index.get', function(t) {
         u('node', {word: 'constructor', id: 1}),
         u('node', {word: 'toString', id: 2})
       ])
-      var $$ = select(ast)
       var index = new Index(ast, 'word')
 
-      sst.deepEqual(index.get('__proto__'), $$('[id=0]'))
-      sst.deepEqual(index.get('constructor'), $$('[id=1]'))
-      sst.deepEqual(index.get('toString'), $$('[id=2]'))
+      sst.deepEqual(index.get('__proto__'), [select('[id=0]', ast)])
+      sst.deepEqual(index.get('constructor'), [select('[id=1]', ast)])
+      sst.deepEqual(index.get('toString'), [select('[id=2]', ast)])
       sst.end()
     })
 
@@ -188,15 +189,14 @@ test('index.get', function(t) {
         u('node', {word: id1, id: 4}),
         u('node', {word: id2, id: 5})
       ])
-      var $$ = select(ast)
       var index = new Index(ast, 'word')
 
-      sst.deepEqual(index.get(false), $$('[id=0]'))
-      sst.deepEqual(index.get('false'), $$('[id=1]'))
-      sst.deepEqual(index.get(1), $$('[id=2]'))
-      sst.deepEqual(index.get('1'), $$('[id=3]'))
-      sst.deepEqual(index.get(id1), $$('[id=4]'))
-      sst.deepEqual(index.get(id2), $$('[id=5]'))
+      sst.deepEqual(index.get(false), [select('[id=0]', ast)])
+      sst.deepEqual(index.get('false'), [select('[id=1]', ast)])
+      sst.deepEqual(index.get(1), [select('[id=2]', ast)])
+      sst.deepEqual(index.get('1'), [select('[id=3]', ast)])
+      sst.deepEqual(index.get(id1), [select('[id=4]', ast)])
+      sst.deepEqual(index.get(id2), [select('[id=5]', ast)])
       sst.deepEqual(index.get({foo: 'bar'}), [])
       sst.end()
     })
@@ -217,19 +217,18 @@ test('index.get', function(t) {
       u('skip', {word: 'foo'}),
       u('skip', {word: 'bar'})
     ])
-    var $$ = select(ast)
 
     st.test('type test', function(sst) {
       var index = new Index(ast, 'node', 'word')
-      sst.deepEqual(index.get('foo'), $$('node[word="foo"]'))
-      sst.deepEqual(index.get('bar'), $$('node[word="bar"]'))
+      sst.deepEqual(index.get('foo'), [select('node[word="foo"]', ast)])
+      sst.deepEqual(index.get('bar'), [select('node[word="bar"]', ast)])
       sst.end()
     })
 
     st.test('function test', function(sst) {
       var index = new Index(ast, filter, 'word')
-      sst.deepEqual(index.get('foo'), $$('node[word="foo"]'))
-      sst.deepEqual(index.get('bar'), $$('node[word="bar"]'))
+      sst.deepEqual(index.get('foo'), [select('node[word="foo"]', ast)])
+      sst.deepEqual(index.get('bar'), [select('node[word="bar"]', ast)])
       sst.end()
 
       function filter(node, index, parent) {
@@ -247,17 +246,20 @@ test('index.get', function(t) {
       u('node', {x: 3, y: 1, id: 3}),
       u('node', {x: 4, y: 1, id: 4})
     ])
-    var $ = select.one(ast)
     var index
 
     index = new Index(ast, xPlusY)
-    st.deepEqual(index.get(4), [$('[id=0]'), $('[id=2]'), $('[id=3]')])
+    st.deepEqual(index.get(4), [
+      select('[id=0]', ast),
+      select('[id=2]', ast),
+      select('[id=3]', ast)
+    ])
     st.deepEqual(index.get(0), [])
-    st.deepEqual(index.get(5), [$('[id=1]'), $('[id=4]')])
+    st.deepEqual(index.get(5), [select('[id=1]', ast), select('[id=4]', ast)])
 
     st.deepEqual(new Index(ast, 'node', xPlusY).get(4), [
-      $('[id=2]'),
-      $('[id=3]')
+      select('[id=2]', ast),
+      select('[id=3]', ast)
     ])
 
     st.end()
@@ -276,23 +278,25 @@ test('index.remove', function(t) {
     u('node', {word: 'foo'}),
     u('node', {word: 'bar'})
   ])
-  var $ = select.one(ast)
 
   var index = new Index(ast, 'word')
-  t.deepEqual(index.get('foo'), [$('bad[word=foo]'), $('node[word=foo]')])
+  t.deepEqual(index.get('foo'), [
+    select('bad[word=foo]', ast),
+    select('node[word=foo]', ast)
+  ])
 
-  var result = index.remove($('bad'))
-  t.deepEqual(index.get('foo'), [$('node[word=foo]')])
+  var result = index.remove(select('bad', ast))
+  t.deepEqual(index.get('foo'), [select('node[word=foo]', ast)])
 
   t.equal(result, index, 'returns this')
 
-  index.remove($('bad'))
-  t.deepEqual(index.get('foo'), [$('node[word=foo]')])
+  index.remove(select('bad', ast))
+  t.deepEqual(index.get('foo'), [select('node[word=foo]', ast)])
 
   index.remove(u('terrible', {word: 'baz'}))
-  t.deepEqual(index.get('foo'), [$('node[word=foo]')])
+  t.deepEqual(index.get('foo'), [select('node[word=foo]', ast)])
 
-  index.remove($('node[word=foo]'))
+  index.remove(select('node[word=foo]', ast))
   t.deepEqual(index.get('foo'), [])
 
   t.end()
